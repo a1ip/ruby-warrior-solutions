3e4ba1c2e867d7a070db1bc1056e1ae5f75f5d17
Casey Robinson <kc@rampantmonkey.com>
2013-04-26 11:33:36 -0400

    Complete Beginner Level 6

--- player.rb
+++ player.rb
@@ -1,9 +1,11 @@
 class Player
   def initialize
-    @dying_health = 8
+    @dying_health  = 8
     @severe_health = 13
-    @max_health   = 20
-    @last_health  = 20
+    @max_health    = 20
+    @last_health   = 20
+    @direction     = :backward
+    @retreating    = false
   end
 
   def play_turn(warrior)
@@ -22,7 +24,7 @@ class Player
 
 
   private
-    attr_reader :w, :dying_health, :max_health, :last_health, :severe_health
+    attr_reader :w, :dying_health, :max_health, :last_health, :severe_health, :direction
 
     def setup warrior
       @w = warrior
@@ -34,16 +36,28 @@ class Player
     end
 
     def pick_action
-      if w.feel.enemy?
+      if retreating?
+        if taking_damage?
+          :walk!
+        elsif not injured?
+          :end_retreat
+        else
+          heal
+        end
+      elsif feel.enemy?
         :attack!
-      elsif w.feel.empty?
+      elsif feel.empty?
         if severe? and healable?
           heal
+        elsif dying? and not healable?
+          :begin_retreat
         else
           :walk!
         end
-      elsif w.feel.captive?
+      elsif feel.captive?
         :rescue!
+      elsif feel.wall?
+        :turn_around
       end
     end
 
@@ -51,11 +65,39 @@ class Player
       case action
       when :not_healable
         @healable = false
+      when :turn_around
+        turn
+      when :begin_retreat
+        turn
+        @retreating = true
+      when :end_retreat
+        turn
+        @retreating = false
       end
     end
 
-    def perform action
-      w.send action
+    def retreating?
+      @retreating
+    end
+
+    def feel
+      w.feel direction
+    end
+
+    def turn
+      if direction == :forward
+        @direction = :backward
+      else
+        @direction = :forward
+      end
+    end
+
+    def perform action, dir=direction
+      if %i[walk! attack! rescue!].include? action
+        w.send action, dir
+      else
+        w.send action
+      end
     end
 
     def dying?
