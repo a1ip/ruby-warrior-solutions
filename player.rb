8b32e2f3f2e1b02dcf470f6f448590a482909eec
Casey Robinson <kc@rampantmonkey.com>
2013-04-26 11:09:57 -0400

    Complete Beginner Level 4

--- player.rb
+++ player.rb
@@ -1,12 +1,13 @@
 class Player
   def initialize
     @dying_health = 8
+    @severe_health = 13
     @max_health   = 20
+    @last_health  = 20
   end
 
   def play_turn(warrior)
-    @w = warrior
-
+    setup warrior
 
     next_action = pick_action
     until next_action[-1] == "!"
@@ -15,17 +16,28 @@ class Player
     end
 
     perform next_action
+
+    cleanup
   end
 
 
   private
-    attr_reader :w, :dying_health, :max_health
+    attr_reader :w, :dying_health, :max_health, :last_health, :severe_health
+
+    def setup warrior
+      @w = warrior
+      @healable = true
+    end
+
+    def cleanup
+      @last_health = w.health
+    end
 
     def pick_action
       if w.feel.enemy?
         :attack!
       elsif w.feel.empty?
-        if dying?
+        if severe? and healable?
           heal
         else
           :walk!
@@ -34,6 +46,10 @@ class Player
     end
 
     def update action
+      case action
+      when :not_healable
+        @healable = false
+      end
     end
 
     def perform action
@@ -48,7 +64,19 @@ class Player
       w.health < max_health
     end
 
+    def severe?
+      w.health < severe_health
+    end
+
     def heal
-      injured? ? :rest! : :none
+      (injured? and not taking_damage?) ? :rest! : :not_healable
+    end
+
+    def healable?
+      @healable
+    end
+
+    def taking_damage?
+      w.health < last_health
     end
 end
