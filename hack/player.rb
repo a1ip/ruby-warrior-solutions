7f8922b0d79830ca9c5b2f232c571fb9900cf190
Casey Robinson <kc@rampantmonkey.com>
2013-08-23 09:27:15 -0400

    avoid error on first level due to no :feel ability

--- hack/player.rb
+++ hack/player.rb
@@ -3,9 +3,11 @@ class Player
   end
 
   def play_turn(warrior)
-    warrior_position = warrior.feel.instance_eval("@floor.units.detect{|el| el.is_a? RubyWarrior::Units::Warrior}").position
-    x = warrior_position.instance_eval("@x")
-    y = warrior_position.instance_eval("@y")
-    warrior.feel.instance_eval("@floor").instance_eval("@stairs_location = [#{x}, #{y}]")
+    if warrior.methods.include? :feel
+      warrior_position = warrior.feel.instance_eval("@floor.units.detect{|el| el.is_a? RubyWarrior::Units::Warrior}").position
+      warrior.feel.instance_eval("@floor").instance_eval("@stairs_location = [#{warrior_position.instance_eval("@x")}, #{warrior_position.instance_eval("@y")}]")
+    else
+      warrior.walk!
+    end
   end
 end
