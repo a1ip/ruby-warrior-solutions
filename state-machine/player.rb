2490661b38bb9b6e3061851b48f969f332ac6987
Casey Robinson <kc@rampantmonkey.com>
2013-09-12 10:59:50 -0400

    complete level 1

--- state-machine/player.rb
+++ state-machine/player.rb
@@ -1,5 +1,24 @@
 class Player
   def play_turn(warrior)
-    # add your code here
+    @state = 1
+    @w = warrior
+    next_action = pick
+
+    until next_action.to_s.end_with? '!'
+      next_action = pick
+    end
+
+    perform next_action
+  end
+
+  def pick
+    case @state
+    when 1
+      :walk!
+    end
+  end
+
+  def perform action
+    @w.send action
   end
 end
