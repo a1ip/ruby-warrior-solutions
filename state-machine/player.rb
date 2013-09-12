3aaa060139dceea5ed02a27ca1b25949b9fc0697
Casey Robinson <kc@rampantmonkey.com>
2013-09-12 11:21:46 -0400

    complete level 4

--- state-machine/player.rb
+++ state-machine/player.rb
@@ -1,17 +1,23 @@
 class Player
-  MAX_HEALTH = 20
+  MAX_HEALTH   = 20
+  DYING_HEALTH = 10
+
+  def initialize
+    @last_health = MAX_HEALTH
+  end
 
   def play_turn(warrior)
-    @state = 4
+    @state = 10
     @w = warrior
-    next_action = pick
+    next_action =  Array pick
 
-    until next_action.to_s.end_with? '!'
+    until next_action.first.to_s.end_with? '!'
       update next_action
-      next_action = pick
+      next_action = Array pick
     end
 
     perform next_action
+    @last_health = @w.health
   end
 
   def pick
@@ -28,6 +34,16 @@ class Player
       :feel
     when 6
       :rest!
+    when 7
+      :rest!
+    when 8
+      %i[walk! backward]
+    when 9
+      %i[feel backward]
+    when 10
+      :health
+    when 11
+      :health
     end
   end
 
@@ -37,17 +53,35 @@ class Player
     when 2
       @state = value.empty? ? 1 : 3
     when 4
-      @state = injured?(value) ? 5 : 2
+      @state = injured?(value) ? 6 : 2
     when 5
-      @state = value.empty? ? 6 : 3
+      @state = value.empty? ? 4 : 3
+    when 9
+      @state = value.wall? ? 7 : 8
+    when 10
+      @state = dying?(value) ? 9 : 11
+    when 11
+      @state = taking_damage?(value) ? 2 : 5
     end
   end
 
   def perform action
-    @w.send action
+    if action.length > 1
+      @w.send action.first, action[1]
+    else
+      @w.send action.first
+    end
   end
 
   def injured? health
     health < MAX_HEALTH
+  end
+
+  def dying? health
+    health < DYING_HEALTH
+  end
+
+  def taking_damage? health
+    health < @last_health
   end
 end
